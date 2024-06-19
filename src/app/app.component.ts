235ba89ee97b87b3b388d8aa9df9be841538a258
Naime Ahmed <naime.ahmed@metadesignsolutions.co.uk>
2024-06-19 21:29:07 +0600

    scroll to top done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { setTheme } from 'ngx-bootstrap/utils';
+import { ScrollTopComponent } from './shared/components/scroll-top/scroll-top.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ScrollTopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/shared/components/scroll-top/scroll-top.component.ts
@@ -0,0 +1,37 @@
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
+
+
+@Component({
+  selector: 'app-scroll-top',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './scroll-top.component.html',
+  styleUrl: './scroll-top.component.scss'
+})
+export class ScrollTopComponent {
+  windowScrolled: boolean = false;
+
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    if (window.scrollY || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+      this.windowScrolled = true;
+    }
+    else if (this.windowScrolled && window.scrollY || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+      this.windowScrolled = false;
+    }
+  }
+
+  scrollToTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth"
+    });
+  }
+
+  ngOnInit() {
+  }
+}
