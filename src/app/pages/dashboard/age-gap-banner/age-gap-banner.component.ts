9b64a82d1adab2f84089a77cf19f588430d815d0
Naime Ahmed <naime.ahmed@metadesignsolutions.co.uk>
2024-06-23 21:15:37 +0600

    bioage age gap animation added

--- src/app/pages/dashboard/age-gap-banner/age-gap-banner.component.ts
+++ src/app/pages/dashboard/age-gap-banner/age-gap-banner.component.ts
@@ -1,19 +1,25 @@
 import { trigger, transition, style, animate } from '@angular/animations';
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { heartBeat, flipInX, slideInLeft } from '../../../shared/animation';
 import { CommonModule } from '@angular/common';
 import { HeartComponent } from './heart/heart.component';
+import gsap from 'gsap';
+
 
 @Component({
   selector: 'app-age-gap-banner',
   standalone: true,
-  imports: [
-    CommonModule,
-    HeartComponent
-  ],
+  imports: [CommonModule, HeartComponent],
   templateUrl: './age-gap-banner.component.html',
   styleUrl: './age-gap-banner.component.scss',
-  animations:[
+  animations: [
     trigger(`flyInDown`, [
       transition('void => *', [
         style({ opacity: 0, transform: `translateY(-50%)` }),
@@ -32,30 +38,87 @@ import { HeartComponent } from './heart/heart.component';
         ),
       ]),
     ]),
-    heartBeat(1300, 1200),
+    heartBeat(1300, 1500),
     flipInX(1000, 100),
-    slideInLeft(1000, 100)
-  ]
+    slideInLeft(1000, 100),
+  ],
 })
-export class AgeGapBannerComponent implements OnInit, OnChanges {
+export class AgeGapBannerComponent implements OnInit, OnChanges, AfterViewInit {
   description =
-  'Research has shown a 5 year difference between BioAge and chronological age is associated with an increased risk of conditions like cardiovascular disease';
-@Input() yourAge!: number;
-@Input() yourBioAge!: number;
+    'Research has shown a 5 year difference between BioAge and chronological age is associated with an increased risk of conditions like cardiovascular disease';
+  @Input() yourAge!: number;
+  @Input() yourBioAge!: number;
 
-ageDifference!: number;
-ageDifferenceAbs!: number;
+  ageDifference!: number;
+  ageDifferenceAbs!: number;
 
-ngOnInit(): void {
-  this.setAgeDifference();
-}
-ngOnChanges(changes: SimpleChanges): void {
-  this.setAgeDifference();
-}
-setAgeDifference(){
-  this.ageDifference = this.yourBioAge - this.yourAge;
-  this.ageDifferenceAbs = Math.abs(this.ageDifference);
-  this.ageDifferenceAbs =
-    this.ageDifferenceAbs > 5 ? 5 : this.ageDifferenceAbs;
-}
+  ngOnInit(): void {
+    this.setAgeDifference();
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.setAgeDifference();
+  }
+  setAgeDifference() {
+    this.ageDifference = this.yourBioAge - this.yourAge;
+    this.ageDifferenceAbs = Math.abs(this.ageDifference);
+    this.ageDifferenceAbs =
+      this.ageDifferenceAbs > 5 ? 5 : this.ageDifferenceAbs;
+  }
+
+  ngAfterViewInit(): void {
+    let tl = gsap.timeline();
+    tl.from('.brick.bg-1' , {
+      x: -500,
+      opacity: 0,
+      duration: 0.3,
+      ease: "power2.inOut"
+    })
+
+    tl.from('.brick.bg-2' , {
+      x: -500,
+      opacity: 0,
+      duration: 0.3,
+      ease: "power2.inOut"
+    })
+    tl.from('.brick.bg-3' , {
+      x: -500,
+      opacity: 0,
+      duration: 0.3,
+      ease: "power2.inOut"
+    })
+    tl.from('.brick.bg-4' , {
+      x: -500,
+      opacity: 0,
+      duration: 0.3,
+      ease: "power2.inOut"
+    })
+    tl.from('.brick.bg-5' , {
+      x: -500,
+      opacity: 0,
+      duration: 0.3,
+      ease: "power2.inOut"
+    })
+    tl.from('.brick.bg-6' , {
+      x: -500,
+      opacity: 0,
+      duration: 0.3,
+      ease: "power2.inOut"
+    });
+
+    gsap.from('.summary', {
+      x: -200,
+      opacity: 0,
+      duration: 1.5,
+      ease: "power1.inOut"
+    })
+
+    gsap.from('.description', {
+      x: 200,
+      opacity: 0,
+      duration: 1.5,
+      ease: "power1.inOut"
+    })
+  }
+
+
 }
